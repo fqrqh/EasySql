b10c7f774c9d0420cb0084a47e9163ed8262e2ab
fqrqh <fisousiphone@gmail.com>
2026-09-25 04:11:46 +0200

    Update main.C

--- main.C
+++ main.C
@@ -1,1 +1,13 @@
 
+#include <stdio.h>
+
+#define GREEN   "\033[32m"
+#define RESET   "\033[0m"
+
+int main() {
+    char name[] = "User"; 
+
+    printf("%sHello %s!\n%s", GREEN, name, RESET);
+
+    return 0;
+}
